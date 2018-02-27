fbf464a73c7f31e12f1c39edff53e7d30ea45d77
Jofemago <jofemago@utp.edu.co>
2018-02-27 10:07:28 -0500

    actualizacion

--- Clases/ComponentesSeriesFourier/PruebaFourier.py
+++ Clases/ComponentesSeriesFourier/PruebaFourier.py
@@ -5,11 +5,16 @@ T = 2
 w0 = sym.pi
 t, n = sym.symbols('t n', real = True)
 #n =  sym.symbols = ('n')
-
+print ('ao=')
 #declaramos la integral
 a0 = (sym.integrate(t+1,(t,-1,0)) + sym.integrate(-t+1 , (t,0,1)))/T
 
 sym.pprint(a0)
 
+print ('an=')
 an = 2 * (sym.integrate((t+1)*sym.cos(n * w0 * t), (t,-1,0))+ sym.integrate((-t +1)*sym.cos(n *w0*t),(t,0,1)))/T
 sym.pprint(an)
+
+print ('bn=')
+bn=2*(sym.integrate((t+1)*sym.sin(n*w0*t), (t, -1, 0)) + sym.integrate((-t+1)*sym.sin(n*w0*t), (t, 0, 1)))/T
+sym.pprint(bn)
